3f29df896b7028529932585f651f5116e18a3217
Ivan <Ivan@ACER>
2016-11-09 18:52:33 +0500

    1

--- IIS_Costumes/Program.cs
+++ IIS_Costumes/Program.cs
@@ -9,6 +9,7 @@ namespace IIS_Costumes
     static class Program
     {
         /// <summary>
+        /// Hello!
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
